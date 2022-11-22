bf4a4286a8f5c7928123bf74872a4431288450cd
unknown <affanshaikh24898@gmail.com>
2022-11-22 04:58:05 -0800

    hello2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
                         {{-- <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a> --}}
                         <a href="{{ url('/login') }}" class="text-sm text-gray-700 underline">Logout</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login1</a>
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
                         @if (Route::has('register'))
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
