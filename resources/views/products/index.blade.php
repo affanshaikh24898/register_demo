826716c976f6f9c44314cba1c6e53c950f7db517
unknown <affanshaikh24898@gmail.com>
2022-12-21 18:44:15 +0530

    name change

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,10 +1,10 @@
 @extends('products.layout')
-     
+
 @section('content')
     <div class="row">
         <div class="col-lg-12 margin-tb">
             <div class="pull-left">
-                <h2>Laravel 8 CRUD Example</h2>
+                <h2>Products 8 CRUD Example</h2>
             </div>
             <div class="pull-right">
                 <a class="btn btn-success" href="{{ route('products.create') }}"> Create New Product</a>
@@ -12,16 +12,16 @@
             </div>
         </div>
     </div>
-    
+
     @if ($message = Session::get('success'))
         <div class="alert alert-success">
             <p>{{ $message }}
             <button type="button" class="close" data-dismiss="alert">×</button>
             </p>
-            
+
         </div>
     @endif
-     
+
     <table class="table table-bordered">
         <tr>
             <th>No</th>
@@ -46,21 +46,21 @@
             <td>{{ $product->status }}</td>
             <td>
                 <form action="{{ route('products.destroy',$product->id) }}" method="POST">
-     
+
                     <a class="btn btn-info" href="{{ route('products.show',$product->id) }}">Show</a>
-      
+
                     <a class="btn btn-primary" href="{{ route('products.edit',$product->id) }}">Edit</a>
-     
+
                     @csrf
                     @method('DELETE')
-        
+
                     <button type="submit" class="btn btn-danger">Delete</button>
                 </form>
             </td>
         </tr>
         @endforeach
     </table>
-    
+
     {!! $products->links() !!}
-        
+
 @endsection
